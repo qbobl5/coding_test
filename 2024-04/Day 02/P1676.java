ec62d6f7ab9b41abae1cb1b427cc5fe70cd3e6b1
qbobl5 <113408131+qbobl5@users.noreply.github.com>
2024-04-02 22:31:26 +0900

    feat: coding test

---
+++ 2024-04/Day 02/P1676.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		System.out.println(N/5+N/25+N/125);
+	}
+
+}
