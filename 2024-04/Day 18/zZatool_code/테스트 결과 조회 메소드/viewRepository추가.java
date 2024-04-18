9601ff53490829addc7dc15277c56316bc7be93c
qbobl5 <113408131+qbobl5@users.noreply.github.com>
2024-04-19 02:54:36 +0900

    feat: side project

---
+++ 2024-04/Day 18/zZatool_code/테스트 결과 조회 메소드/viewRepository추가.java
@@ -0,0 +1,7 @@
+package com.timekiller.zzatool.test.dao;
+
+import com.timekiller.zzatool.test.entity.View;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ViewRepository extends JpaRepository<View, Long> {}
