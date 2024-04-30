f6c6222395a29dfd1401e7fd79f07a497dde346d
qbobl5 <113408131+qbobl5@users.noreply.github.com>
2024-04-30 22:30:51 +0900

    feat: coding test

---
+++ 2024-04/Day 30/P17219.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
+		
+		Map<String, String> map=new HashMap<>();
+		
+		for(int i=0;i<N;i++) {
+			st=new StringTokenizer(br.readLine());
+			map.put(st.nextToken(), st.nextToken());
+		}
+		
+		for(int i=0;i<M;i++) {
+			String url=br.readLine();
+			String pwd=map.get(url);
+			bw.write(pwd+"\n");
+		}
+		
+		bw.close();
+		br.close();
+	}
+
+}
